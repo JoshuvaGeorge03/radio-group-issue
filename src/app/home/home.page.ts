523c17929aa03ba24d89d7ea8120fe1a62d6b00c
joshuva george <joshuva@adaptivemedia.in>
2019-10-22 11:36:41 +0530

    updated

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,302 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public newProductsAndVariants = [
+    {
+      languages: {
+        en: {
+          name: 'Fade',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut a earum natus, aliquam quaerat, eaque omnis placeat saepe eum rerum ipsa facere dolore! Modi commodi impedit tempora ratione nihil. Cum.'
+        }
+      },
+      variants: [
+        {
+          languages: {
+            en: {
+              variantName: '3mm Fade'
+            }
+          },
+          duration: '25',
+          pricing: [
+            {
+              price: 350
+            }
+          ]
+        }
+      ]
+    },
+    {
+      languages: {
+        en: {
+          name: 'Fade',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut a earum natus, aliquam quaerat, eaque omnis placeat saepe eum rerum ipsa facere dolore! Modi commodi impedit tempora ratione nihil. Cum.'
+        }
+      },
+      variants: [
+        {
+          languages: {
+            en: {
+              variantName: '3mm Fade'
+            }
+          },
+          duration: '25',
+          pricing: [
+            {
+              price: 350
+            }
+          ]
+        }
+      ]
+    },
+    {
+      languages: {
+        en: {
+          name: 'Fade',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut a earum natus, aliquam quaerat, eaque omnis placeat saepe eum rerum ipsa facere dolore! Modi commodi impedit tempora ratione nihil. Cum.'
+        }
+      },
+      variants: [
+        {
+          languages: {
+            en: {
+              variantName: '3mm Fade'
+            }
+          },
+          duration: '25',
+          pricing: [
+            {
+              price: 350
+            }
+          ]
+        }
+      ]
+    },{
+      languages: {
+        en: {
+          name: 'Fade',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut a earum natus, aliquam quaerat, eaque omnis placeat saepe eum rerum ipsa facere dolore! Modi commodi impedit tempora ratione nihil. Cum.'
+        }
+      },
+      variants: [
+        {
+          languages: {
+            en: {
+              variantName: '3mm Fade'
+            }
+          },
+          duration: '25',
+          pricing: [
+            {
+              price: 350
+            }
+          ]
+        }
+      ]
+    },
+    {
+      languages: {
+        en: {
+          name: 'Fade',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut a earum natus, aliquam quaerat, eaque omnis placeat saepe eum rerum ipsa facere dolore! Modi commodi impedit tempora ratione nihil. Cum.'
+        }
+      },
+      variants: [
+        {
+          languages: {
+            en: {
+              variantName: '3mm Fade'
+            }
+          },
+          duration: '25',
+          pricing: [
+            {
+              price: 350
+            }
+          ]
+        }
+      ]
+    },
+    {
+      languages: {
+        en: {
+          name: 'Fade',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut a earum natus, aliquam quaerat, eaque omnis placeat saepe eum rerum ipsa facere dolore! Modi commodi impedit tempora ratione nihil. Cum.'
+        }
+      },
+      variants: [
+        {
+          languages: {
+            en: {
+              variantName: '3mm Fade'
+            }
+          },
+          duration: '25',
+          pricing: [
+            {
+              price: 350
+            }
+          ]
+        }
+      ]
+    },
+    {
+      languages: {
+        en: {
+          name: 'Fade',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut a earum natus, aliquam quaerat, eaque omnis placeat saepe eum rerum ipsa facere dolore! Modi commodi impedit tempora ratione nihil. Cum.'
+        }
+      },
+      variants: [
+        {
+          languages: {
+            en: {
+              variantName: '3mm Fade'
+            }
+          },
+          duration: '25',
+          pricing: [
+            {
+              price: 350
+            }
+          ]
+        }
+      ]
+    },{
+      languages: {
+        en: {
+          name: 'Fade',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut a earum natus, aliquam quaerat, eaque omnis placeat saepe eum rerum ipsa facere dolore! Modi commodi impedit tempora ratione nihil. Cum.'
+        }
+      },
+      variants: [
+        {
+          languages: {
+            en: {
+              variantName: '3mm Fade'
+            }
+          },
+          duration: '25',
+          pricing: [
+            {
+              price: 350
+            }
+          ]
+        }
+      ]
+    },
+    {
+      languages: {
+        en: {
+          name: 'Fade',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut a earum natus, aliquam quaerat, eaque omnis placeat saepe eum rerum ipsa facere dolore! Modi commodi impedit tempora ratione nihil. Cum.'
+        }
+      },
+      variants: [
+        {
+          languages: {
+            en: {
+              variantName: '3mm Fade'
+            }
+          },
+          duration: '25',
+          pricing: [
+            {
+              price: 350
+            }
+          ]
+        }
+      ]
+    },
+    {
+      languages: {
+        en: {
+          name: 'Fade',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut a earum natus, aliquam quaerat, eaque omnis placeat saepe eum rerum ipsa facere dolore! Modi commodi impedit tempora ratione nihil. Cum.'
+        }
+      },
+      variants: [
+        {
+          languages: {
+            en: {
+              variantName: '3mm Fade'
+            }
+          },
+          duration: '25',
+          pricing: [
+            {
+              price: 350
+            }
+          ]
+        }
+      ]
+    },{
+      languages: {
+        en: {
+          name: 'Fade',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut a earum natus, aliquam quaerat, eaque omnis placeat saepe eum rerum ipsa facere dolore! Modi commodi impedit tempora ratione nihil. Cum.'
+        }
+      },
+      variants: [
+        {
+          languages: {
+            en: {
+              variantName: '3mm Fade'
+            }
+          },
+          duration: '25',
+          pricing: [
+            {
+              price: 350
+            }
+          ]
+        }
+      ]
+    },{
+      languages: {
+        en: {
+          name: 'Fade',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut a earum natus, aliquam quaerat, eaque omnis placeat saepe eum rerum ipsa facere dolore! Modi commodi impedit tempora ratione nihil. Cum.'
+        }
+      },
+      variants: [
+        {
+          languages: {
+            en: {
+              variantName: '3mm Fade'
+            }
+          },
+          duration: '25',
+          pricing: [
+            {
+              price: 350
+            }
+          ]
+        }
+      ]
+    },{
+      languages: {
+        en: {
+          name: 'Fade',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut a earum natus, aliquam quaerat, eaque omnis placeat saepe eum rerum ipsa facere dolore! Modi commodi impedit tempora ratione nihil. Cum.'
+        }
+      },
+      variants: [
+        {
+          languages: {
+            en: {
+              variantName: '3mm Fade'
+            }
+          },
+          duration: '25',
+          pricing: [
+            {
+              price: 350
+            }
+          ]
+        }
+      ]
+    }
+  ];
   constructor() {}
 
 }
